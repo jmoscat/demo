9e3d1d1c06b657dda84bca538e495d9a6417cbb1
Chris Nixon <chris.d.nixon@gmail.com>
2012-10-07 14:40:19 -0700

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Demo::Application.routes.draw do
   #get "live/get_live_tweets"
-  match 'live/index' => 'live#index'
-  match '/live/get_live_tweets' => 'live#get_live_tweets'
+  scope :path => '/live', :controller => :live do
+    match 'index' => :index
+    match 'get_live_tweets' => :get_live_tweets
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +54,7 @@ Demo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'live#index'
 
   # See how all your routes lay out with "rake routes"
 
